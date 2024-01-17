a3a492b3f8d4bdfd08088fc16d8e716bcca60c90
심은주 <nyaknya@naver.com>
2024-01-17 18:25:46 +0900

    피드백 반영

--- src/utils/calculateDate.js
+++ src/utils/calculateDate.js
@@ -1,53 +1,53 @@
-const second = 1000;
-const minute = second * 60;
-const hour = minute * 60;
-const day = hour * 24;
-const month = day * 31;
-const year = month * 12;
+const SECOND = 1000;
+const MINUTE = SECOND * 60;
+const HOUR = MINUTE * 60;
+const DAY = HOUR * 24;
+const MONTH = DAY * 31;
+const YEAR = MONTH * 12;
 
 const TIME_IN_MILLISECONDS = {
-  second,
-  minute,
-  hour,
-  day,
-  month,
-  year,
+  SECOND,
+  MINUTE,
+  HOUR,
+  DAY,
+  MONTH,
+  YEAR,
 };
 
 const getElapsedTime = (createdAt) => {
   const now = new Date();
   const createdAtDate = new Date(createdAt);
   const elapsedTime = now - createdAtDate;
-  const { minute, hour, day, month, year } = TIME_IN_MILLISECONDS;
+  const { MINUTE, HOUR, DAY, MONTH, YEAR } = TIME_IN_MILLISECONDS;
 
-  if (year * 2 <= elapsedTime) {
-    return `${Math.floor(elapsedTime / year)} years ago`;
+  if (YEAR * 2 <= elapsedTime) {
+    return `${Math.floor(elapsedTime / YEAR)} YEARs ago`;
   }
-  if (year <= elapsedTime) {
-    return `1 year ago`;
+  if (YEAR <= elapsedTime) {
+    return `1 YEAR ago`;
   }
-  if (month * 2 <= elapsedTime) {
-    return `${Math.floor(elapsedTime / month)} months ago`;
+  if (MONTH * 2 <= elapsedTime) {
+    return `${Math.floor(elapsedTime / MONTH)} MONTHs ago`;
   }
-  if (month <= elapsedTime) {
-    return `1 month ago`;
+  if (MONTH <= elapsedTime) {
+    return `1 MONTH ago`;
   }
-  if (day * 2 <= elapsedTime) {
-    return `${Math.floor(elapsedTime / day)} days ago`;
+  if (DAY * 2 <= elapsedTime) {
+    return `${Math.floor(elapsedTime / DAY)} DAYs ago`;
   }
-  if (day <= elapsedTime) {
-    return `1 day ago`;
+  if (DAY <= elapsedTime) {
+    return `1 DAY ago`;
   }
-  if (hour * 2 <= elapsedTime) {
-    return `${Math.floor(elapsedTime / hour)} hours ago`;
+  if (HOUR * 2 <= elapsedTime) {
+    return `${Math.floor(elapsedTime / HOUR)} HOURs ago`;
   }
-  if (hour <= elapsedTime) {
-    return `1 hour ago`;
+  if (HOUR <= elapsedTime) {
+    return `1 HOUR ago`;
   }
-  if (minute * 2 <= elapsedTime) {
-    return `${Math.floor(elapsedTime / minute)} minutes ago`;
+  if (MINUTE * 2 <= elapsedTime) {
+    return `${Math.floor(elapsedTime / MINUTE)} MINUTEs ago`;
   }
-  return `1 minute ago`;
+  return `1 MINUTE ago`;
 };
 
 export default getElapsedTime;
