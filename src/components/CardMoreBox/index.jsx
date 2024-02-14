d404970f159a055e7eff85f8fa92a04ffd332f70
심은주 <nyaknya@naver.com>
2024-02-15 04:29:07 +0900

    feat: 드롭다운 구현

---
+++ src/components/CardMoreBox/index.jsx
@@ -0,0 +1,14 @@
+import "./style.css";
+
+function CardMoreBox() {
+  return (
+    <div className="kebab-more-box">
+      <ul>
+        <li>삭제하기</li>
+        <li>폴더에 추가</li>
+      </ul>
+    </div>
+  );
+}
+
+export default CardMoreBox;
--- src/components/FolderComponents/FolderCard/index.jsx
+++ src/components/FolderComponents/FolderCard/index.jsx
@@ -1,8 +1,29 @@
 import { Link } from "react-router-dom";
+import CardMoreBox from "../../CardMoreBox/index.jsx";
 import getElapsedTime from "../../../utils/calculateDate.js";
 import "./style.css";
+import { useEffect, useRef, useState } from "react";
 
 function Card({ links }) {
+  const [kebabVisible, setKebabVisible] = useState(false);
+
+  const dropDownRef = useRef();
+
+  useEffect(() => {
+    const handleOutsideClose = (e) => {
+      if (kebabVisible && !dropDownRef.current.contains(e.target))
+        setKebabVisible(false);
+    };
+    document.addEventListener("click", handleOutsideClose);
+
+    return () => document.removeEventListener("click", handleOutsideClose);
+  }, [kebabVisible]);
+
+  const handleKebabVisible = (e) => {
+    e.preventDefault();
+    setKebabVisible(!kebabVisible);
+  };
+
   const { title, description, url, image_source, created_at } = links;
 
   const timeAgo = getElapsedTime(created_at);
@@ -11,13 +32,27 @@ function Card({ links }) {
   return (
     <>
       <Link to={url} target="_blank">
+        <div className="star-button">
+          <img src="/assets/star.svg" alt="즐겨찾기 버튼" />
+        </div>
         <div className="card-imgbox">
           <img src={image_source ?? "/assets/defalutimg.png"} alt={title} />
         </div>
         <div className="card-content">
-          <time className="time-ago" datatime={created_at}>
-            {timeAgo}
-          </time>
+          <div className="time-box">
+            <time className="time-ago" datatime={created_at}>
+              {timeAgo}
+            </time>
+            <div className="more-box" ref={dropDownRef}>
+              <img
+                src="/assets/kebab.svg"
+                alt="더보기 버튼"
+                onClick={handleKebabVisible}
+              />
+              {kebabVisible ? <CardMoreBox /> : null}
+            </div>
+          </div>
+
           <p>{description}</p>
           <time className="creat-date" datatime={created_at}>
             {creatDate}
